fe76f4ef3d427a2a570c58f73a54b435d66dc4d5
oguhpereira <oguhpereira@protonmail.com>
2022-03-14 21:59:17 -0300

    feat: create tutorial app in v4

---
+++ v4/src/App.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+import { PostList, PostEdit, PostCreate, PostShow } from "./resources/Posts";
+import { UserList } from "./resources/Users";
+import Dashboard from "./components/Dashbord";
+import authProvider from "./authProvider";
+
+const App = () => (
+  <Admin
+    dataProvider={jsonServerProvider("https://jsonplaceholder.typicode.com")}
+    authProvider={authProvider}
+    dashboard={Dashboard}
+  >
+    <Resource
+      name="posts"
+      icon={PostIcon}
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      show={PostShow}
+    />
+    <Resource name="users" icon={UserIcon} list={UserList} />
+    <Resource name="comments" list={ListGuesser} />
+  </Admin>
+);
+export default App;
